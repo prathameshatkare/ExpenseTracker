794333c420b14de9c9f682b7cb910eaee42bb45a
PRATHAMESHATKARE <142227723+prathameshatkare@users.noreply.github.com>
2025-04-06 17:54:40 +0530

    Update wsgi.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+path = '/home/asus/expense_tracker'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'expense_tracker.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
